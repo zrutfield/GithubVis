558ef8a84ce05e609be3974001905924e3a37e7c
Zackary Rutfield <zrutfield@gmail.com>
2016-04-25 17:58:30 -0400

    Data is now pulled in chunks

--- gui.py
+++ gui.py
@@ -1,16 +1,17 @@
 import sys
-import csv
-import random
-import json
+#import csv
+#import random
+#import json
 from datetime import datetime
 
 from github import Github
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui  # , QtCore
 import pyqtgraph as pg
 import numpy as np
 from scipy import interpolate
 
-from repo import Repo, Issue, Commit
+from repo import Repo
+
 
 class Window(QtGui.QWidget):
     def __init__(self):
@@ -19,11 +20,11 @@ class Window(QtGui.QWidget):
         #pg.setConfigOption('background', 'w')
         #pg.setConfigOption('foreground', 'k')
 
-        
-        print(self.github.rate_limiting)
+        #print(self.github.rate_limiting)
+        self.github = Github( )  # Need to put in authentication
 
         self.createPlots()
-        
+
         self.createUI()
 
     def createPlots(self):
@@ -31,20 +32,19 @@ class Window(QtGui.QWidget):
         self.issuesPlot = self.layout.addPlot(row=0, col=0)
         self.issuesCurve = self.issuesPlot.plot()
         self.commitsPlot = self.layout.addPlot(row=1, col=0)
-        
 
     def createUI(self):
         self.repoEdit = QtGui.QLineEdit(self)
         self.repoStart = QtGui.QPushButton("Start", self)
         self.repoStart.clicked.connect(self.createRepo)
-        
+
         self.repoLabel = QtGui.QLabel(self)
         self.repoLabel.setText("Enter Repository:")
 
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(self.repoEdit)
         hbox.addWidget(self.repoStart)
-        
+
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(self.repoLabel)
         vbox.addLayout(hbox)
@@ -59,15 +59,17 @@ class Window(QtGui.QWidget):
         print("Getting repo: ", self.repoEdit.text())
         self.repo = Repo(self.repoEdit.text(), self.github)
         print("Got data")
-        self.processIssuesData()
-        print("Process")
-        self.updateIssuesPlot()
+        self.repo.issue_processed.connect(self.processIssuesData)
+        #self.processIssuesData()
+        #print("Process")
+        #self.updateIssuesPlot()
         print("Plotted")
-
 
     def processIssuesData(self):
         dates = {}
         issues = self.repo.issues
+        if len(issues) == 0:
+            return
         for issue in issues:
             create = issue.created_at
             if create in dates:
@@ -81,7 +83,7 @@ class Window(QtGui.QWidget):
                     dates[close] -= 1
                 else:
                     dates[close] = -1
-        total = 0
+        #total = 0
         unixDT = datetime(1970, 1, 1)
         self.issuesData = [[], []]
         for date in dates.items():
@@ -89,27 +91,30 @@ class Window(QtGui.QWidget):
             self.issuesData[1].append(date[1])
         self.issuesData = [list(x) for x in zip(*sorted(zip(self.issuesData[0], self.issuesData[1]), key=lambda p: p[0]))]
         for i in range(1, len(self.issuesData[1])):
-            self.issuesData[1][i] += self.issuesData[1][i-1]
+            self.issuesData[1][i] += self.issuesData[1][i - 1]
+        print("Process")
+        self.updateIssuesPlot()
+        print("Plotted")
 
     def updateIssuesPlot(self):
-        #print(self.issuesData)
-        sl = self.smoothLine(self.issuesData[0], self.issuesData[1])
+        #sl = self.smoothLine(self.issuesData[0], self.issuesData[1])
+        self.issuesCurve.clear()
         self.issuesCurve.setData(x=self.issuesData[0], y=self.issuesData[1], pen='r')
         #self.issuesCurve.setData(x=sl[0], y=sl[1], pen='r')
 
     def smoothLine(self, x, y):
         spline = interpolate.UnivariateSpline(x, y)
-        x1 = np.linspace(min(x), max(x), num=len(x)*100)
+        x1 = np.linspace(min(x), max(x), num=len(x) * 100)
         y1 = spline(x1)
         return (x1, y1)
-    
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
     w = Window()
     w.show()
     sys.exit(app.exec_())
-    
-    
+
+
 if __name__ == "__main__":
     main()
--- repo.py
+++ repo.py
@@ -3,40 +3,55 @@ from PyQt4 import QtCore
 
 
 class Repo(QtCore.QObject):
+    commit_processed = QtCore.pyqtSignal()
+    issue_processed = QtCore.pyqtSignal()
     def __init__(self, _name, _gh=None):
+        QtCore.QObject.__init__(self)
         self.name = _name
-        self.commit_processed = QtCore.Signal()
-        self.issue_processed = QtCore.Signal()
         if _gh is None:
             self.read_cache()  # If no github object is passed in, look in cache
         else:
-            repo = _gh.get_repo(self.name)
+            self.repo = _gh.get_repo(self.name)
             #self.commits = [self.pull_commits(repo)
             self.commits = []
             self.issues = []
+            self.commit_page = 0
+            self.issue_page = 0
 
-            self.pull_commits(repo)
-            self.pull_issues(repo)
+            self.commit_timer = QtCore.QTimer()
+            self.commit_timer.setInterval(1000)
+            self.commit_timer.timeout.connect(self.pull_commits)
+            self.commit_timer.start()
 
-    def pull_commits(self, repo):
-        commits_list = repo.get_commits()
-        page_count = 0
-        page = commits_list.get_page(page_count)
-        while len(page) > 0:
+            self.issue_timer = QtCore.QTimer()
+            self.issue_timer.setInterval(200)
+            self.issue_timer.timeout.connect(self.pull_issues)
+            self.issue_timer.start()
+            #self.pull_commits(repo)
+            #self.pull_issues(repo)
+
+    def pull_commits(self):
+        commits_list = self.repo.get_commits()
+        page = commits_list.get_page(self.commit_page)
+        if len(page) == 0:
+            self.commit_timer.stop()
+            return
+        else:
             self.commits += [Commit(commit) for commit in page]
-            page_count += 1
-            page = commits_list.get_page(page_count)
             self.commit_processed.emit()
+            self.commit_page += 1
 
-    def pull_issues(self, repo):
-        issues_list = repo.get_issues(state='all')
-        page_count = 0
-        page = issues_list.get_page(page_count)
-        while len(page) > 0:
+    def pull_issues(self):
+        print("ISSUE TIMER TIMEOUT")
+        issues_list = self.repo.get_issues(state='all')
+        page = issues_list.get_page(self.issue_page)
+        if len(page) == 0:
+            self.issue_timer.stop()
+            return
+        else:
             self.issues += [Issue(issue) for issue in page]
-            page_count += 1
-            page = issues_list.get_page(page_count)
             self.issue_processed.emit()
+            self.issue_page += 1
 
     def read_cache(self):
         pass  # TODO: Implement cache
